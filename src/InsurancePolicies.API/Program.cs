bcf12ddf7edf9bb351b0e7f6e839a48f5d6137db
edwin.jimenez <edwin.jimenez@aplicando.co>
2023-06-02 14:25:16 -0500

    modification program.cs

--- src/InsurancePolicies.API/Program.cs
+++ src/InsurancePolicies.API/Program.cs
@@ -7,9 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-// ENV PREFIX AND DOMAIN
-
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
